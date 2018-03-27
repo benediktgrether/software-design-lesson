d25760d5abe49c6fda1963ace24039b50dc8dda1
Benedikt Grether <benedikt@pixelarbeit.de>
2018-03-27 12:24:07 +0200

    Starting C# project - L02

---
+++ L02/X01_types/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace X01_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horst; 
+            horst = 42;
+            Console.WriteLine(horst * 2 );
+            Console.WriteLine("Hello World!");
+
+            // String count to know how many bytes the string need it
+            string j = "Hello, World";
+            var countByte = j.Length * sizeof(Char);
+            Console.WriteLine(j + " need "  + countByte + " Bytes ");
+        
+        // System.Text.ASCIIEncoding.ASCII.GetByteCount(j);   
+        }
+    }
+}
