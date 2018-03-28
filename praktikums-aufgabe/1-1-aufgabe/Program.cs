9f13d691f68ca7426822b087b0176587f6d42b60
Benedikt Grether <benedikt@pixelarbeit.de>
2018-03-28 14:15:22 +0200

    Work on getCubeInfoW Methode

--- praktikums-aufgabe/1-1-aufgabe/Program.cs
+++ praktikums-aufgabe/1-1-aufgabe/Program.cs
@@ -9,28 +9,47 @@ namespace _1_1_aufgabe
             string w = "w";
             string k = "k";
             string o = "o";
+            double a;
+            double v;
             var eingabe = (args[0]);
             var Num = (args[1]); 
             double d = Convert.ToDouble(Num);
             // double d = Convert.ToDouble(test);
              if (w == eingabe){
-                Console.WriteLine("Hello World");
-                Console.WriteLine(d.GetType() + " " + d);
-                double a = 6 * (d * d);
-                double v = d * d * d;
-                Console.WriteLine(a + " " + v);
+                a = getCubeSurfaceW(d);
+                v = getCubeVolumeW(d);
+                getCubeInfoW(a, v);
+                // Console.WriteLine(a);
+                // getCubeInfoW(a, v);
             }else if( k == eingabe ){
-
+                // getCubeSurfaceK(d);
+                // getCubeVolumeK(d);
             }else if( o == eingabe ){
             
             }else{
                 Console.WriteLine("Wrong input");
             }
+
+            // A = getCubeSurfaceW(a);
             
         }
     
-        public static double getCubeSurface(double d){
-            
+        public static double getCubeSurfaceW(double kanten){
+            double d = kanten;
+            double a = 6 * (d * d);
+            // Console.WriteLine(a);
+            return a;
+        }
+        public static double getCubeVolumeW(double kanten){
+            double d = kanten;
+            double v = d * d * d;
+            return v;
+        }
+
+        public static void getCubeInfoW(double surface, double volume){
+            double a = surface;
+            double v = volume;
+            Console.WriteLine(a + " | " + v );
         }
     }
 
