edd6c45fbc2e275fdf9b21d63117429a554bd547
Benedikt Grether <benedikt@pixelarbeit.de>
2018-03-28 12:36:21 +0200

    Work on Convert to Double

--- praktikums-aufgabe/1-1-aufgabe/Program.cs
+++ praktikums-aufgabe/1-1-aufgabe/Program.cs
@@ -6,14 +6,32 @@ namespace _1_1_aufgabe
     {
         static void Main(string[] args)
         {
-            // string name;
-            // Console.WriteLine("TEST");
-            // name = Console.ReadLine();
+            string w = "w";
+            string k = "k";
+            string o = "o";
+            var eingabe = (args[0]);
+            var Num = (args[1]); 
+            double d = Convert.ToDouble(Num);
+            // double d = Convert.ToDouble(test);
+             if (w == eingabe){
+                Console.WriteLine("Hello World");
+                Console.WriteLine(d.GetType() + " " + d);
+                // double a = 6 * (d * d);
+                // double v = d * d * d;
+                // Console.WriteLine(a + " " + v);
+            }else if( k == eingabe ){
+
+            }else if( o == eingabe ){
+            
+            }else{
+                Console.WriteLine("Wrong input");
+            }
+            
         }
 
-        public double Oberflaeche(double kantenlangen, double durchmesser){
-            double a = 6 * kantenlangen * kantenlangen;
-            return a;
-        }
+        // public double Oberflaeche(double kantenlangen, double durchmesser){
+        //     double a = 6 * kantenlangen * kantenlangen;
+        //     return a;
+        // }
     }
 }
