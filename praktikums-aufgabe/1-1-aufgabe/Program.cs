d7a666ef56c2e3bc01b26606485660c568990733
Benedikt Grether <benedikt@pixelarbeit.de>
2018-03-28 21:33:21 +0200

    finish excercise 1-1

--- praktikums-aufgabe/1-1-aufgabe/Program.cs
+++ praktikums-aufgabe/1-1-aufgabe/Program.cs
@@ -9,47 +9,65 @@ namespace _1_1_aufgabe
             string w = "w";
             string k = "k";
             string o = "o";
-            double a;
-            double v;
             var eingabe = (args[0]);
             var Num = (args[1]); 
             double d = Convert.ToDouble(Num);
-            // double d = Convert.ToDouble(test);
              if (w == eingabe){
-                a = getCubeSurfaceW(d);
-                v = getCubeVolumeW(d);
-                getCubeInfoW(a, v);
-                // Console.WriteLine(a);
-                // getCubeInfoW(a, v);
+                getCubeInfo(d);
             }else if( k == eingabe ){
-                // getCubeSurfaceK(d);
-                // getCubeVolumeK(d);
+                getSphereInfo(d);
             }else if( o == eingabe ){
-            
+                getSphereInfo(d);
             }else{
                 Console.WriteLine("Wrong input");
-            }
-
-            // A = getCubeSurfaceW(a);
-            
+            }            
         }
-    
-        public static double getCubeSurfaceW(double kanten){
-            double d = kanten;
-            double a = 6 * (d * d);
-            // Console.WriteLine(a);
+        // Cube
+        public static double getCubeSurface(double durchmesser){
+            double d = durchmesser;
+            double a = Math.Round(6 * (d * d), 2);
             return a;
         }
-        public static double getCubeVolumeW(double kanten){
-            double d = kanten;
-            double v = d * d * d;
+        public static double getCubeVolume(double durchmesser){
+            double d = durchmesser;
+            double v = Math.Round(d * d * d, 2);
             return v;
         }
+        public static void getCubeInfo(double durchmesser){
+            double d = durchmesser;
+            Console.WriteLine("Kubus: A=" + getCubeSurface(d) + " | V=" + getCubeVolume(d) );
+        }
 
-        public static void getCubeInfoW(double surface, double volume){
-            double a = surface;
-            double v = volume;
-            Console.WriteLine(a + " | " + v );
+        // Sphere
+        public static double getSphereSurface(double durchmesser){
+            double d = durchmesser;
+            double a = Math.Round(Math.PI * ( d * d), 2);
+            return a;
+        }
+        public static double getSphereVolume(double durchmesser){
+            double d = durchmesser;
+            double v = Math.Round(Math.PI * ( d * d * d) / 6 , 2);
+            return v;
+        }
+        public static void getSphereInfo(double durchmesser){
+            double d = durchmesser;
+            Console.WriteLine("Kugel: A=" + getSphereSurface(d) + " | V=" + getSphereVolume(d) );
+        }
+        
+        // Octahedron
+        public static double getOctahedronSurface(double durchmesser){
+            double d = durchmesser;
+            double a = Math.Round(2 *( Math.Sqrt(3)) * d * d, 2);
+            return a;
+        }
+        public static double getOctahedronVolume(double durchmesser){
+            double d = durchmesser;
+            double v = Math.Round(Math.Sqrt(2) * d * d * d / 3 , 2);
+            return v;
+        }
+        public static void getOctahedronInfo(double durchmesser){
+            double d = durchmesser;
+            Console.WriteLine("Oktaeder: A=" +getOctahedronSurface(d) + " | V=" + getOctahedronVolume(d) );
         }
     }
 
