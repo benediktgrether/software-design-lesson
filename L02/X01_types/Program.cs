c2b49f62f86461f8198b0da821f17cd3c4f15612
Benedikt Grether <benedikt@pixelarbeit.de>
2018-03-28 11:56:06 +0200

    Editing

--- L02/X01_types/Program.cs
+++ L02/X01_types/Program.cs
@@ -2,6 +2,11 @@
 
 namespace X01_types
 {
+    public class Person
+    {
+        public string Name;
+        public int Personalnummer;
+    }
     class Program
     {
         static void Main(string[] args)
@@ -16,7 +21,8 @@ namespace X01_types
             var countByte = j.Length * sizeof(Char);
             Console.WriteLine(j + " need "  + countByte + " Bytes ");
         
-        // System.Text.ASCIIEncoding.ASCII.GetByteCount(j);   
+            Console.WriteLine(System.Text.ASCIIEncoding.ASCII.GetByteCount(j) + " Bytes"); 
+            Console.WriteLine(System.Text.ASCIIEncoding.Unicode.GetByteCount(j) + " Bytes");  
         
         // To Do - Initialisierung & Typ Inferenz
 
@@ -35,6 +41,77 @@ namespace X01_types
             Console.WriteLine(i.GetType() + " -- " + i);
             Console.WriteLine(pi.GetType() + " -- " + pi);
             Console.WriteLine(salute.GetType() + " -- " + salute);
+
+            // To Do float declaration with var
+            var db = 3.5f;
+            Console.WriteLine(db.GetType() + " -- " + db + " float value" );
+            //short
+            short shortValue1 = 1_034;
+            Console.WriteLine(shortValue1.GetType() + " -- " + db + " short value" );
+        
+            // Array - Variable declaration
+            int[] ia = {1, 0, 2, 9, 3, 8, 4, 7, 5, 6};
+            int ergebnis = ia[2] * ia[8] + ia[4];
+            // 2 * 5  + 3 
+            // Das Ergebnis ist 13
+            Console.WriteLine("Das Ergebnis ist = " + ergebnis);
+
+            // Array - double ( Pi , Eulersche number , Kepler-constant)
+            double[] ja = {3.145,  2.71828, 2.97E-19};
+            for(int k = 0; k < ja.Length; k++){
+                Console.WriteLine(ja[k]);
+            }
+
+            // Array Length
+            Console.WriteLine("---------------");
+            Console.WriteLine(ja.Length);
+
+            // String 
+            string meinString = "Dies ist ein String";
+            char zeichen = meinString[5];
+            string a = "eins";
+            string b = "zwei";
+            string c = "eins";
+            bool a_eq_b = (a == b);
+            bool a_eq_c = (a == c);
+
+            Console.WriteLine("-------");
+            Console.WriteLine(meinString);
+            Console.WriteLine(zeichen);
+            Console.WriteLine(c);
+            Console.WriteLine(a_eq_b);
+            Console.WriteLine(a_eq_c);
+
+            // class Person
+
+            Person jemand = new Person();
+            jemand.Name = "Horst";
+            jemand.Personalnummer = 42;
+            
+            Person jemand2 = new Person {Name="Sabine", Personalnummer=89};
+
+            Console.WriteLine(jemand.Name + " " + jemand.Personalnummer);
+            Console.WriteLine(jemand2.Name + " " + jemand2.Personalnummer);
+            Console.WriteLine("--------------");
+
+            // if / else operation
+            int ax = int.Parse(Console.ReadLine());
+            int ay = int.Parse(Console.ReadLine());
+
+            if (ay < ax){
+                Console.WriteLine(ax + " ist größer " + ay);
+            }else{
+                Console.WriteLine(ax + " ist kleiner " + ay);
+            }
+
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (3 < x ){
+                Console.WriteLine();
+            }else{
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ praktikums-aufgabe/1-1-aufgabe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1_1_aufgabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string name;
+            // Console.WriteLine("TEST");
+            // name = Console.ReadLine();
+        }
+
+        public double Oberflaeche(double kantenlangen, double durchmesser){
+            double a = 6 * kantenlangen * kantenlangen;
+            return a;
+        }
+    }
+}
